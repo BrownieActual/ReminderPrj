3c1514f1f3d124ddb81cc1c80d41da6f99a79e2f
jirass <jirass_bark@hotmail.com>
2017-06-13 14:05:09 +0700

    upload a photo and take a photo DONE!!!!!!! But...... not upload to firebase yet.............

--- ReminderPrj/app/src/main/java/com/example/alvin/reminderprj/addActivity.java
+++ ReminderPrj/app/src/main/java/com/example/alvin/reminderprj/addActivity.java
@@ -1,9 +1,12 @@
 package com.example.alvin.reminderprj;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -11,9 +14,12 @@ import android.widget.ImageView;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.IOException;
+
 public class addActivity extends AppCompatActivity {
     private StorageReference mStorageRef;
-    private static final int MY_REQUEST = 100;
+    private static final int UPLOAD_PHOTO = 100;
+    private static final int TAKE_PHOTO = 200;
     private String titleString;
     private String descriptionString;
     private Bitmap imgString;
@@ -44,20 +50,64 @@ public class addActivity extends AppCompatActivity {
     }
 
     public void SelectImage(View view) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, MY_REQUEST);
-        }
+        CharSequence picker[] = new CharSequence[] {"Upload a photo", "Take a photo"};
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose an action");
+        builder.setItems(picker, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                switch(which){
+                    case 0:{
+                        Intent intent = new Intent();
+                        intent.setType("image/*");
+                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                        if (intent.resolveActivity(getPackageManager()) != null) {
+                            startActivityForResult(intent, UPLOAD_PHOTO);
+                        }
+                        break;
+                    }
+                    case 1:{
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        if (intent.resolveActivity(getPackageManager()) != null) {
+                            startActivityForResult(intent, TAKE_PHOTO);
+                        }
+                        break;
+                    }
+                    default:{
+                        break;
+                    }
+                }
+            }
+        });
+        builder.show();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bundle extra = data.getExtras();
-        Bitmap bitmap = (Bitmap) extra.get("data");
-        ImageView imageView = (ImageView) findViewById(R.id.editImage);
-        imageView.setImageBitmap(bitmap);
+//        upload photo
+        if (requestCode == UPLOAD_PHOTO && resultCode == RESULT_OK) {
+                Uri uri = data.getData();
+
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+                    ImageView imageView = (ImageView) findViewById(R.id.editImage);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+//        take photo
+        if (requestCode == TAKE_PHOTO && resultCode == RESULT_OK) {
+            Bundle extra = data.getExtras();
+            Bitmap bitmap = (Bitmap) extra.get("data");
+            ImageView imageView = (ImageView) findViewById(R.id.editImage);
+            imageView.setImageBitmap(bitmap);
+        }
     }
+
 }
 
