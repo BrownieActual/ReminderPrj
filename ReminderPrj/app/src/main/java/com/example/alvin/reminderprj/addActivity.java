2298d5721c2fefb4593d0fe9c032693b71197fe5
jirass <jirass_bark@hotmail.com>
2017-06-13 12:58:02 +0700

    add take a picture

--- ReminderPrj/app/src/main/java/com/example/alvin/reminderprj/addActivity.java
+++ ReminderPrj/app/src/main/java/com/example/alvin/reminderprj/addActivity.java
@@ -2,12 +2,18 @@ package com.example.alvin.reminderprj;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
-public class addActivity extends AppCompatActivity {
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+public class addActivity extends AppCompatActivity {
+    private StorageReference mStorageRef;
+    private static final int MY_REQUEST = 100;
     private String titleString;
     private String descriptionString;
     private Bitmap imgString;
@@ -17,20 +23,41 @@ public class addActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         setTitle("Add");
-    }
-    private void add(){
-
-    }
-    private void discard(){
-
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 
-    private void toMainActivity(){
+    private void add() {
+
+    }
+
+    private void discard() {
+
+    }
+
+    private void toMainActivity() {
         Intent intent = new Intent(addActivity.this, mainActivity.class);
         startActivity(intent);
     }
-    private void clearFields(){
+
+    private void clearFields() {
 
     }
 
+    public void SelectImage(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, MY_REQUEST);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle extra = data.getExtras();
+        Bitmap bitmap = (Bitmap) extra.get("data");
+        ImageView imageView = (ImageView) findViewById(R.id.editImage);
+        imageView.setImageBitmap(bitmap);
+    }
 }
+
